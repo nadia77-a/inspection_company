18e1f51faf2010a2c978a9e24dbbc23ac10a206f
nadia <nadia@altechsolutions.al>
2020-11-27 18:38:42 +0100

    ch

--- src/components/Quality/index.js
+++ src/components/Quality/index.js
@@ -45,6 +45,77 @@ class Quality extends React.Component {
               commitments and regulatory requirements achieved.
             </p>
           </div>
+          <div className="description__row">
+            <div className="testimonial center">
+              <h3>
+                <strong>Services</strong>
+              </h3>
+              <p>
+                • Strategic Planning • Management Review • Controlled Document
+                System Verification • Data Collection Control • Corrective and
+                Preventative Action Monitoring • Risk Assessment Analysis •
+                Continual Improvement Process
+              </p>
+            </div>
+          </div>
+          <div className="description__row">
+            <div className="description__row__col2">
+              <h3>
+                <strong>INTERSICA Auditors</strong>
+              </h3>
+              <p>
+                During the INTERSICA (formerly Socotec Oil&Gas) employment
+                process all INTERSICA auditors are vetted verifying competencies
+                and qualifications as well as implementation of any additional
+                training needed for continual improvement. Auditors are held to
+                INTERSICA’s standards while conducting your business, including
+                reporting and HSE guidelines. During employment all auditors
+                undergo regular on-site evaluations and mandatory training.
+              </p>
+            </div>
+            <div className="description__row__col2">
+              <img
+                src="https://intersica.com/wp-content/uploads/2019/07/inspectors.jpg"
+                alt=""
+              ></img>
+            </div>
+            <div className="description__row__col2">
+              <h3>
+                <strong>Reporting</strong>
+              </h3>
+              <p>
+                INTERSICA (formerly Socotec Oil&Gas) personnel are dedicated to
+                providing the best possible service. A crucial element of this
+                is the reporting process, which enables our clients to fully
+                understand the overall status of their projects. Reports can be
+                prepared on our own reporting format or client specific
+                formatting if required. Training on proper report writing is
+                mandatory with all INTERSICA auditors to ensure sufficient
+                information is captured and relayed back to our customers.
+              </p>
+            </div>
+          </div>
+          <div className="description__row">
+            <p>
+              <i>
+                » Having issues with your vendor? Need to audit a supplier prior
+                to adding them to your approved vendor list? INTERSICA has
+                certified auditors with the knowledge and experience to perform
+                audits on your suppliers to ensure that they meet or exceed your
+                requirements. «
+              </i>
+            </p>
+          </div>
+          <div className="description__row">
+            <div className="description__row__col3">
+              <h4>test</h4>
+              <p>test descr</p>
+            </div>
+            <div className="description__row__col3">
+              <h4>test</h4>
+              <p>test descr</p>
+            </div>
+          </div>
         </div>
       </div>
     );
