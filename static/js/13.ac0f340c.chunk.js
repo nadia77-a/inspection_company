8a27700ffdcd7e293b659adca386020687b4a791
nadia <nadia@altechsolutions.al>
2021-02-03 12:58:53 +0100

    Updates

---
+++ static/js/13.ac0f340c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbaseapp=this.webpackJsonpbaseapp||[]).push([[13],{232:function(e,t,n){"use strict";n.r(t);var a=n(41),r=n(42),u=n(44),c=n(43),p=n(0),s=n.n(p),i=function(e){Object(u.a)(n,e);var t=Object(c.a)(n);function n(){return Object(a.a)(this,n),t.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return s.a.createElement("div",null,"Logout")}}]),n}(s.a.Component);t.default=i}}]);
+//# sourceMappingURL=13.ac0f340c.chunk.js.map
