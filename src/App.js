0aeda3261b49a66a058ef41f085ba689158762de
nadia77-a <nadiaarapi@hotmail.com>
2021-02-21 23:49:24 +0100

    ch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import { Provider } from "react-redux";
 // import store from "./redux-store/store";
 import Root from "./routes-config";
+import "./style.css";
 
 function App() {
   return <Root />;
--- src/shared-components/Header/index.js
+++ src/shared-components/Header/index.js
@@ -3,7 +3,15 @@ import "./style.css";
 import allimages from "config/images";
 
 class Header extends Component {
+
+state={
+  isOpen:false
+}
+
   render() {
+
+    const{isOpen}=this.state;
+
     return (
       <header id="main-header" className="">
         <div className="container et_menu_container">
@@ -13,6 +21,54 @@ class Header extends Component {
             </a>
           </div>
           <div>
+
+         
+              <div id="et_mobile_nav_menu">
+                <div className={`mobile_nav `+ (isOpen ? "":"closed")}>
+                  <span className="mobile_menu_bar mobile_menu_bar_toggle" onClick={()=>this.setState({isOpen:!isOpen})}>
+                    <i className="fa fa-bars"></i>
+                  </span>
+
+                  <ul id="top-menu" className="nav">
+                <li className="menu-item">
+                  <a href="./#">Home</a>
+                </li>
+                <li className="menu-item">
+                  <a href="./#about">About</a>
+                </li>
+                <li className="menu-item">
+                  <a href="./#contact">Contact</a>
+                </li>
+                <li className="menu-item">
+                  <a href="./#" className="hasCh">Services</a>
+                  <ul className="sub-menu">
+                    <li className="menu-item">
+                      <a href="./#design/">Design</a>
+                    </li>
+                    <li className="menu-item">
+                      <a href="./#inspection-expediting/">
+                        Inspection and Expediting
+                      </a>
+                    </li>
+                    <li className="menu-item">
+                      <a href="./#welding/">Welding</a>
+                    </li>
+                    <li className="menu-item">
+                      <a href="./#quality-support/">Quality Support</a>
+                    </li>
+                    <li className="menu-item">
+                      <a href="./#oil-gas/">Oil and Gas</a>
+                    </li>
+                  </ul>
+                </li>
+                {/* <li className="menu-item">
+                  <a href="./#career">Career</a>
+                </li> */}
+              </ul>
+                </div>
+              </div>{" "}
+         
+
             <nav id="top-menu-nav">
               <ul id="top-menu" className="nav">
                 <li className="menu-item">
@@ -31,7 +87,9 @@ class Header extends Component {
                       <a href="./#design/">Design</a>
                     </li>
                     <li className="menu-item">
-                      <a href="./#inspection-expediting/">Inspection and Expediting</a>
+                      <a href="./#inspection-expediting/">
+                        Inspection and Expediting
+                      </a>
                     </li>
                     <li className="menu-item">
                       <a href="./#welding/">Welding</a>
@@ -40,9 +98,7 @@ class Header extends Component {
                       <a href="./#quality-support/">Quality Support</a>
                     </li>
                     <li className="menu-item">
-                      <a href="./#oil-gas/">
-                        Oil and Gas
-                      </a>
+                      <a href="./#oil-gas/">Oil and Gas</a>
                     </li>
                   </ul>
                 </li>
