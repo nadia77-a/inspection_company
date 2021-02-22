84dca8cf8de808aab770dd4867a286337f1b878c
nadia77-a <nadiaarapi@hotmail.com>
2021-02-23 00:51:46 +0100

    ch

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,11 +13,20 @@ class Contact extends React.Component {
             </div>
           </div>
         </div>
+        <div className="who centerContent">
+          <p>very project is an open ended enquiry, h desired one. We provide guidance, expertise and experience.very project is an open ended enquiry, helping people to challenge their existing state and work towards an alternative, desired one. We provide guidance, expertise and experience.</p>
+          <div className="c1">
+            <img src="https://lh3.googleusercontent.com/proxy/W5m6I9D5Y7e6imu5p_hGIt34LAMAPXQNqA_5dJPq4n3r6SloWlanj_J7-2LVPnHFWCsL53fiNDc0rS4e3eFfBBA3oUVPL2yzGgFHcxooyy2fFMw6cEs0ZHJ2tMYHJVVdfrS9Ieqn_6VEh-vMmFm045qeF8Y"></img>
+          </div>
+          <div className="c2">very project ihallenge their existing state and workne. We provide guidance, expertise and experience.very project is an open ended enquiry, helping people to challenge their existing state and work towards an alternative, desired one. We provide guidance, expertise and experience.</div>
+          </div>
         <div className="centerContent description contact">
+         
+        
           <div className="details">
           <div className="location-box"> <a className="media-left pull-left"> <i className="fa fa-map-marked"></i></a>
 							  <div className="media-body"> <strong>OUR OFFICE LOCATION</strong>
-								<p>rruga Shyqyri Brari, Tirane, Albaniia</p>
+								<p>rruga Shyqyri Brari, Tirane, Albania</p>
 							  </div>
 							</div>
               <div className="location-box"> <a className="media-left pull-left"> <i className="fa fa-map-marked"></i></a>
@@ -31,7 +40,15 @@ class Contact extends React.Component {
 							  </div>
 							</div>
           </div>
-          <div className="map">map</div>
+          <div className="map">
+          <div className="mapouter">
+            <div className="gmap_canvas">
+            <iframe height="342" id="gmap_canvas" src="https://maps.google.com/maps?q=rruga%20shyqyr%20brari&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <a href="https://yt2.org/youtube-to-mp3-ALeKk00qEW0sxByTDSpzaRvl8WxdMAeMytQ1611842368056QMMlSYKLwAsWUsAfLipqwCA2ahUKEwiikKDe5L7uAhVFCuwKHUuFBoYQ8tMDegUAQCSAQCYAQCqAQdnd3Mtd2l6"></a>
+  
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     );
