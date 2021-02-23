6394ed9d7586f1d40df2c6b1d51d7625dca746fc
nadia77-a <nadiaarapi@hotmail.com>
2021-02-24 00:09:22 +0100

    ch

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,105 +1,86 @@
 import React from "react";
 
 import "./style.css";
+import images from "config/images";
 
 class Quality extends React.Component {
+state ={
+  isActive:"home"
+}
+
   render() {
+    const {isActive}=this.state;
     return (
-      <div className="main-content quality">
+      <div className="main-content projects">
         <div className="main-content__top">
           <div className="addShadow">
             <div className="header-content">
               <div className="main-content__top__title centerContent">
-                OUR OFFICE LOCATION
+                DESIGN
                 <div className="description">
-                COMPASS ENGINEERING is an independent technical service company that has expert knowledge and experience of Engineering Services - Expediting - Testing & Inspection - NDT - Welding and Welder Qualification - Painting and Coating - Design - Construction Supervision. Not limited to integrity evaluation, 3rd party manufacturing inspections, supplier quality surveillance, by serving oil & gas companies. Capability of delivering highly qualified expertise with the intention of exceeding our client’s expectations for safety, quality, functionality and deliver services that stand the test of time.
+                  COMPASS ENGINEERING provides engineering design, on-site
+                  services to the oil & gas processing and petrochemical
+                  sectors. Our team of professionals have direct industry
+                  experience that adds significant value at each project stage.
+                  We are uniquely suited to provide responsive support.
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="centerContent description">
-          <div className="description__row">
-            <p>
-              A quality management system (QMS) is a set of policies, processes
-              and procedures required for planning and execution (production/
-              development/ service) in the core business area of an
-              organization. COMPASS(formerly Socotec Oil&Gas) is dedicated to
-              providing value-added resources no mater your requirements and
-              expectations. COMPASS(formerly Socotec Oil&Gas) provides
-              properly vetted, experienced and highly qualified technical
-              auditors from an extensive global pool of talent and guarantees
-              exceptional performance.
-            </p>
-            <p>
-              Why do you need OUR OFFICE LOCATION? With growing changes to
-              procedures and policies, compliance, conformance, and performance
-              will not coincide with your QMS. Auditing of risk and performance,
-              KPI’s and follow up on completed corrective actions serve as a
-              major role in keeping quality programs, company policies, contract
-              commitments and regulatory requirements achieved.
-            </p>
-          </div>
-          <div className="description__row">
-            <div className="testimonial center">
-              <h3>
-                <strong>Services</strong>
-              </h3>
+          <ul class="tabs">
+            <li data-tab-target="#home" class={`tab `+ (isActive==="home" ? "active":"")} onClick={()=>this.setState({isActive:"home"})}>
+              Home
+            </li>
+            <li data-tab-target="#pricing" class={`tab `+ (isActive==="pricing" ? "active":"")} onClick={()=>this.setState({isActive:"pricing"})}>
+              Pricing
+            </li>
+            <li data-tab-target="#about" class={`tab `+ (isActive==="about" ? "active":"")} onClick={()=>this.setState({isActive:"about"})}>
+              About
+            </li>
+            <li data-tab-target="#news" class={`tab `+ (isActive==="news" ? "active":"")} onClick={()=>this.setState({isActive:"news"})}>
+              News
+            </li>
+          </ul>
+
+          <div class="tab-content">
+            <div id="home" data-tab-content class={(isActive==="home" ? "active":"")} onClick={()=>this.setState({isActive:"home"})}>
+              <h1>Home</h1>
               <p>
-                • Strategic Planning • Management Review • Controlled Document
-                System Verification • Data Collection Control • Corrective and
-                Preventative Action Monitoring • Risk Assessment Analysis •
-                Continual Improvement Process
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
               </p>
             </div>
-          </div>
-          <div className="description__row">
-            <div className="description__row__col2">
-              <h3>
-                <strong>COMPASSAuditors</strong>
-              </h3>
+            <div id="pricing" data-tab-content  class={(isActive==="pricing" ? "active":"")} onClick={()=>this.setState({isActive:"pricing"})}>
+              <h1>Pricing</h1>
               <p>
-                During the COMPASS(formerly Socotec Oil&Gas) employment
-                process all COMPASSauditors are vetted verifying competencies
-                and qualifications as well as implementation of any additional
-                training needed for continual improvement. Auditors are held to
-                COMPASS standards while conducting your business, including
-                reporting and HSE guidelines. During employment all auditors
-                undergo regular on-site evaluations and mandatory training.
+              <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
               </p>
             </div>
-            <div className="description__row__col2">
-              <img
-                src="https://intersica.com/wp-content/uploads/2019/07/inspectors.jpg"
-                alt=""
-              ></img>
-            </div>
-            <div className="description__row__col2">
-              <h3>
-                <strong>Reporting</strong>
-              </h3>
+            <div id="about" data-tab-content class={(isActive==="about" ? "active":"")} onClick={()=>this.setState({isActive:"about"})}>
+              <h1>About</h1>
               <p>
-                COMPASS(formerly Socotec Oil&Gas) personnel are dedicated to
-                providing the best possible service. A crucial element of this
-                is the reporting process, which enables our clients to fully
-                understand the overall status of their projects. Reports can be
-                prepared on our own reporting format or client specific
-                formatting if required. Training on proper report writing is
-                mandatory with all COMPASSauditors to ensure sufficient
-                information is captured and relayed back to our customers.
+              <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
               </p>
             </div>
-          </div>
-          <div className="description__row">
-            <p className="text-center">
-              <i>
-                » Having issues with your vendor? Need to audit a supplier prior
-                to adding them to your approved vendor list? COMPASShas
-                certified auditors with the knowledge and experience to perform
-                audits on your suppliers to ensure that they meet or exceed your
-                requirements. «
-              </i>
-            </p>
+            <div id="news" data-tab-content class={(isActive==="news" ? "active":"")} onClick={()=>this.setState({isActive:"news"})}>
+              <h1>News</h1>
+              <p>
+               <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+                <img src="https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg"></img>
+              </p>
+            </div>
           </div>
         </div>
       </div>
