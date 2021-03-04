3e24b6dd1c258c0c97ec14d8e79fd19a0d3563e3
nadia <nadia@altechsolutions.al>
2021-03-04 18:22:05 +0100

    ch

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,10 +2,17 @@ import React from "react";
 
 import "./style.css";
 import images from "config/images";
-
+import { withRouter } from "react-router-dom";
 class Quality extends React.Component {
   state = {
     isActive: "Design",
+  };
+
+  componentDidMount = () => {
+    console.log("tag");
+    const tag = this.props.match.params.cat;
+    console.log("tag", tag);
+    this.setState({ isActive: tag });
   };
 
   render() {
@@ -26,22 +33,22 @@ class Quality extends React.Component {
             src: images.q2,
           },
           {
-            id: 11,
+            id: 13,
             title: "title1",
             src: images.q3,
           },
           {
-            id: 11,
+            id: 14,
             title: "title1",
             src: images.q4,
           },
           {
-            id: 11,
+            id: 15,
             title: "title1",
             src: images.q5,
           },
           {
-            id: 11,
+            id: 16,
             title: "title1",
             src: images.q2,
           },
@@ -52,24 +59,24 @@ class Quality extends React.Component {
         name: "Inspection and Expediting",
         images: [
           {
-            id: 11,
+            id: 21,
             title: "title1",
             src: images.o1,
           },
           {
-            id: 11,
+            id: 22,
             src: images.o2,
           },
           {
-            id: 11,
+            id: 23,
             src: images.o3,
           },
           {
-            id: 11,
+            id: 24,
             src: images.o4,
           },
           {
-            id: 12,
+            id: 25,
             src: images.o5,
           },
         ],
@@ -80,21 +87,21 @@ class Quality extends React.Component {
         title: "title1",
         images: [
           {
-            id: 11,
+            id: 31,
             src:
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 12,
+            id: 32,
             src:
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 11,
+            id: 33,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 34,
             src: images.q1,
           },
         ],
@@ -110,53 +117,53 @@ class Quality extends React.Component {
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 12,
+            id: 42,
             src:
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 11,
+            id: 43,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 44,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 45,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 46,
             src: images.q1,
           },
         ],
       },
       {
-        id: 1,
+        id: 5,
         name: "Oil and Gas",
         title: "title1",
         images: [
           {
-            id: 11,
+            id: 51,
             src:
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 12,
+            id: 52,
             src:
               "https://quintus.modeltheme.com/wp-content/uploads/2016/02/quintus_pic2.jpg",
           },
           {
-            id: 11,
+            id: 53,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 54,
             src: images.q1,
           },
           {
-            id: 11,
+            id: 55,
             src: images.q1,
           },
         ],
@@ -188,7 +195,10 @@ class Quality extends React.Component {
                   key={index}
                   data-tab-target={`#` + item.name}
                   className={`tab ` + (isActive === item.name ? "active" : "")}
-                  onClick={() => this.setState({ isActive: item.name })}
+                  onClick={() => {
+                    window.location.hash = `about/${item.name}`;
+                    this.setState({ isActive: item.name });
+                  }}
                 >
                   {item.name}
                 </li>
@@ -204,7 +214,7 @@ class Quality extends React.Component {
                   <div key={index} className="tabss">
                     {item.images.map((sub, i) => {
                       return (
-                        <div key={i} className={`tabss__single _` + i}>
+                        <div key={sub.id} className={`tabss__single _` + i}>
                           <img src={sub.src} alt=""></img>
                           <h3>{sub?.title}</h3>
                         </div>
@@ -220,4 +230,4 @@ class Quality extends React.Component {
   }
 }
 
-export default Quality;
+export default withRouter(Quality);
